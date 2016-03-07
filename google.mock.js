4871669b8f26e10f68653082b2ec50390bde1b4e
sikB <sikhovski@icloud.com>
2016-03-07 15:50:25 -0500

    Update/not finished
    js on clicks..small stuff

---
+++ google.mock.js
@@ -0,0 +1,11 @@
+function grid(){
+	var icons = document.getElementById('app-icons');
+	console.log(icons);
+	if(icons.style.visibility == "hidden"){
+		icons.style.visibility = 'visible';
+	}else{
+		icons.style.visibility = 'hidden';
+	}
+}
+
+
